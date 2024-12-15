6d2ee5822e57737f91e2cf51594c364666ad2711
luisff73 <jvrluis@hotmail.com>
2024-12-15 11:40:23 +0100

    despliege a vercel

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -40,7 +40,7 @@ export default function NavLinks() {
             )}
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p> // este parrafo solo se muestra en pantallas grandes
+            <p className="hidden md:block">{link.name}</p> {/*este parrafo solo se muestra en pantallas grandes*/}
           </Link>
         );
       })}
