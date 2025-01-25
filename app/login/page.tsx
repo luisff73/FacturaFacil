ae25a825defd8aa13879f0343b37975aeb4d79ea
luisff73 <jvrluis@hotmail.com>
2025-01-25 19:04:52 +0100

    funcionando ok login y logout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+
+import FacturaFacilLogo from '../ui/factura-facil';
 import LoginForm from '@/app/ui/login-form';
 import { Suspense } from 'react';
 
@@ -6,9 +7,9 @@ export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
+        <div className="flex h-20 w-full items-end rounded-lg bg-green-400 p-3 md:h-36">
+          <div className="flex-grow text-white md:w-36 md:h-36">
+            <FacturaFacilLogo />
           </div>
         </div>
         <Suspense>
---
+++ pages/api/auth/providers.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function providers(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    // Devuelve la lista de proveedores de autenticación
+    res.status(200).json({
+      providers: [
+        { name: 'Google', url: '/auth/google' },
+        { name: 'GitHub', url: '/auth/github' },
+        // Añade más proveedores según sea necesario
+      ],
+    });
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
