acc12f7b69aabcc827ea1475377aded0f9ec37a6
luisff73 <jvrluis@hotmail.com>
2025-01-25 17:16:52 +0100

    raiz

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      // ...
-    },
-  },
-}
+  /* config options here */
+};
 
 export default nextConfig;
