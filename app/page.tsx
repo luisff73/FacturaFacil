21026a05a02c45728a78541f2c009db4c75e1b46
luisff73 <jvrluis@hotmail.com>
2026-02-28 19:47:49 +0100

    modificados nombres graficos

--- app/page.tsx
+++ app/page.tsx
@@ -38,14 +38,14 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-desktop.png"
+            src="/grafico_escritorio.png"
             width={760}
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
-            src="/hero-mobile.png"
+            src="/grafico_movil.png"
             width={560}
             height={620}
             className="block md:hidden"
