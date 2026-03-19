ec155fe45e8de190c41f8073fe29e5ca9f83b361
luisff73 <jvrluis@hotmail.com>
2026-03-19 23:32:10 +0100

    instalado sentry para depuracion de codigo y errores de la app

--- middleware.ts
+++ middleware.ts
@@ -59,5 +59,5 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|monitoring|favicon.ico|.*\\.png$).*)"],
 };
